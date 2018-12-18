d1bdd2c1da2d6043c98159c4bd7421f1deb3c104
cristianogregnanin <cristiano.gregnanin@gmail.com>
2018-12-18 12:36:33 -0800

    es2

--- progetto2/array_manager.php
+++ progetto2/array_manager.php
@@ -38,4 +38,48 @@ class array_manager {
         return $max;
     }
 
+    function search($num) {
+        for ($i = 1; $i < count($this->numbers); $i++) {
+            $number = $this->numbers[$i];
+            if ($number == $num) {
+                return true;
+            }
+        }
+    }
+
+    public function add($number) {
+        array_push($this->numbers, $number);
+    }
+
+    public function add_list($numbers) {
+        for ($i = 0; $i < count($numbers); $i++) {
+            $number = $numbers[$i];
+            array_push($this->numbers, $number);
+        }
+    }
+
+    public function remove_element($num) {
+        for ($i = 0; $i < count($this->numbers); $i++) {
+            $number = $this->numbers[$i];
+            if ($number == $num) {
+                $pos = $i;
+            }
+        }
+
+        for ($i = $pos; $i < count($this->numbers)-1; $i++) {
+            $this->numbers[$i] = $this->numbers[$i + 1];
+        }
+    }
+
+    public function remove_at_index($pos) {
+
+        for ($i = $pos; $i < count($this->numbers)-1; $i++) {
+            $this->numbers[$i] = $this->numbers[$i + 1];
+        }
+    }
+
+    public function add_at_index($number, $pos) {
+        $this->numbers[$pos] = $number;
+    }
+
 }
--- progetto2/index.php
+++ progetto2/index.php
@@ -7,23 +7,39 @@
  */
 include 'array_manager.php';
 
-$numbers = array(1, 432, 12, 3423, 1323, 566);
+$numbers = array(1, 3, 5, 7, 9, 11);
 $array_manager = new array_manager();
 
 $array_manager->setArray($numbers);
 
 $array_manager->print_array();
 
-echo "il massimo è:{$array_manager->print_max()}";
+echo "\nil massimo è:{$array_manager->print_max()}\n\n";
 
-if ($array_manager->search(1323)) {
+if ($array_manager->search(132)) {
     echo "numero trovato";
 } else {
-    echo "numero non trovato";
+    echo "numero non trovato\n";
 }
 
+echo "\nfunzione add\n\n";
+$array_manager->add(62);
+$array_manager->print_array();
 
+echo "\nfunzione add list\n\n";
+$array_manager->add_list(array(13,15));
+$array_manager->print_array();
 
+echo "\nfunzione add at index\n\n";
+$array_manager->add_at_index(96,2);
+$array_manager->print_array();
 
+echo "\nfunzione remove element if found\n\n";
+$array_manager->remove_element(96);
+$array_manager->print_array();
+
+echo "\nfunzione remove element at position\n\n";
+$array_manager->remove_at_index(0);
+$array_manager->print_array();
 
 
