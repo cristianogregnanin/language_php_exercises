f314b479be8ab9745702795dcb9a6d7d342f7a53
cristianogregnanin <cristiano.gregnanin@gmail.com>
2020-11-16 13:09:41 -0800

    esercizio php

---
+++ esercizio_if/index.php
@@ -0,0 +1,17 @@
+<?php
+
+echo "inserisci il primo numero: ";
+$a = readline();
+
+echo "inserisci il secondo numero: ";
+$b = readline();
+
+$c = $a + $b;
+
+echo "\nil risultato di $a + $b è: $c\n";
+
+if ($a>$b){
+    echo "$a è maggiore di $b";
+}else{
+    echo "$b è maggiore di $a";
+}
