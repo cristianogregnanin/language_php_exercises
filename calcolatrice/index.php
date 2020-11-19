1b4de9f59e6645449d811f08ee323125136a8616
cristianogregnanin <cristiano.gregnanin@gmail.com>
2020-11-19 10:51:28 -0800

    calcolatrice OOP

---
+++ calcolatrice/index.php
@@ -0,0 +1,25 @@
+<?php
+
+include 'Calcolatrice.php';
+
+$calcolatrice = new Calcolatrice();
+
+do {
+    echo "inserisci il primo numero ";
+    $calcolatrice->setA(readline());
+
+    echo "inserisci il secondo numero ";
+    $calcolatrice->setB(readline());
+
+    echo "scegli l'operatore \n";
+    $calcolatrice->setOperatore(readline());
+
+    $calcolatrice->calcola();
+
+    $risultato = $calcolatrice->getRisultato();
+    echo "il risultato è: $risultato\n";
+    
+    
+    echo "Vuoi fare un altro calcolo? si/no\n";
+    $scelta = readline();
+} while ($scelta == "si");
