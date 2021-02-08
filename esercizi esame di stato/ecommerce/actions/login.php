f08cef6f3c21cade150baf44ebcf18bcf42ad8fd
Cristiano <cristiano.gregnanin@gmail.com>
2021-02-08 21:33:51 +0100

    ecommerce

---
+++ esercizi esame di stato/ecommerce/actions/login.php
@@ -0,0 +1,8 @@
+<?php
+
+var_dump($_POST);
+
+//select * from users where email == $_POST['email'] and password == $_POST['password']
+
+
+?>
---
+++ esercizi esame di stato/ecommerce/views/login.php
@@ -0,0 +1,25 @@
+<?php ?>
+
+
+<html>
+    <head>
+        <title>Login</title>
+    </head>
+
+    <body>
+
+
+        <form action="../actions/login.php" method="POST">
+
+            <input type="text" name="email" placeholder="Email">
+
+            <input type="password" name="password" placeholder="Password">
+
+            <input type="submit" value="submit">
+
+        </form>
+
+
+    </body>
+
+</html>
